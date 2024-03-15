3f4953edee1222d17d362d0bd57a8b787a8f1b37
Ryan Rugh <ryanrugh@gmail.com>
2024-03-15 17:19:27 -0400

    Initial commit, adding directory page

---
+++ Crown-Clothing/src/components/category-item/category-item.component.jsx
@@ -0,0 +1,16 @@
+import styles from './style.module.scss';
+
+const CategoryItem = ({ category }) => {
+    const { title, imageUrl } = category;
+    return (
+        <div className={styles.categoryContainer}>
+          <div className={styles.backgroundImage} style={{backgroundImage: `url(${imageUrl})`}}/>
+          <div className={styles.categoryBodyContainer}>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+    )
+};
+
+export default CategoryItem;
